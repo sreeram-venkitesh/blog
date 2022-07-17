bb9df8e608347d3a83d917b832a25102e6054f47
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-07-17 14:28:22 +0530

    Ran the scaffold to create notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_17_071125) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_17_085035) do
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_071125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
